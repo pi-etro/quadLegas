c354e02c8b9bc21206f8bbefab1cac4412f4854e
Pietro Di Consolo Gregorio <31605283+pi-etro@users.noreply.github.com>
2020-02-06 22:25:29 -0300

    Update common.py

--- scripts/common.py
+++ scripts/common.py
@@ -11,17 +11,6 @@ def clear():
     system('cls' if name == 'nt' else 'clear')
     return
 
-# texto para metodos nao implementados
-def fazer():
-    print('Nao disponivel!')
-    return
-
-# limpa arquivos temporarios para sair do programa
-def clean_exit():
-    if exists(endereco('data/.fKfIASBfFufCPItc')):
-        remove(endereco('data/.fKfIASBfFufCPItc'))
-    return
-
 # logo do programa
 def logo():
     clear()
@@ -44,10 +33,9 @@ def welcome():
     print(  '                               Bem vindo ao quadLegas!\n' +
             '                  Uma ferramenta para explorar a matricula da UFABC\n')
 
-    print(  '1 Descobrir colegas de turma por RA\n' +
-            '2 Descobrir colegas de turma com os codigos das turmas\n' +
-            '3 Importar pdf de matriculas deferidas\n' +
-            '4 Sair\n')
+    print(  '[1] Descobrir colegas de turma por RA\n' +
+            '[2] Descobrir colegas de turma com os codigos das turmas\n' +
+            '[4] Sair\n')
     return
 
 # conjunto de logo mais welcome
